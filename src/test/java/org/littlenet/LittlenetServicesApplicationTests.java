0e2bf330763ec89f088e32041f61fbe4ba4e6128
Anton Kr <antnjavadev@gmail.com>
2023-10-21 16:45:21 +0300

    empty spring project

---
+++ src/test/java/org/littlenet/LittlenetServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.littlenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittlenetServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
